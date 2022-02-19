7788166c2828dd6131698e52e14958d62c81303b
George Gebbett <george.gebbett1@gmail.com>
2022-02-19 13:06:39 +0000

    clean up client code

--- client.js
+++ client.js
@@ -3,38 +3,31 @@ const VLAD = "Vlad";
 
 const socket = io();
 
+const updatePage = (cat, isComingIn) => {
+    switch (cat) {
+        case ESTRAGON:
+            document.getElementById("estragon-location").innerText = `Estragon is ${isComingIn ? "in!" : "out!"}`;
+            break;
+        case VLAD:
+            document.getElementById("vlad-location").innerText = `Vlad is ${isComingIn ? "in!" : "out!"}`;
+            break;
+    }
+}
 
 socket.on('cat movement', (content) => {
     console.log(content);
 
     let catName = content.name;
 
-    switch (catName) {
-        case ESTRAGON:
-            document.getElementById("estragon-location").innerText = `Estragon is ${content.in ? "in!" : "out!"}`;
-            break;
-        case VLAD:
-            document.getElementById("vlad-location").innerText = `Vlad is ${content.in ? "in!" : "out!"}`;
-            break;
-    }
+    updatePage(catName, content.in);
+
 })
 
 socket.on('cat state', (content) => {
     console.log(content);
 
     for (const cat of content.catState) {
-        let catName = cat.name;
-        switch (catName) {
-            case ESTRAGON:
-                document.getElementById("estragon-location").innerText = `Estragon is ${cat.in ? "in!" : "out!"}`;
-                break;
-            case VLAD:
-                document.getElementById("vlad-location").innerText = `Vlad is ${cat.in ? "in!" : "out!"}`;
-                break;
-        }
+        updatePage(cat.name, cat.in)
     }
-
-
-
 
 })
