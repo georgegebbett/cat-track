b12f439669596f06954183fb8f9b0d47c79de26d
George Gebbett <george.gebbett1@gmail.com>
2022-02-21 12:50:54 +0000

    store everything in a db

--- client.js
+++ client.js
@@ -3,31 +3,25 @@ const VLAD = "Vlad";
 
 const socket = io();
 
-const updatePage = (cat, isComingIn) => {
+const updatePage = (cat, isComingIn, updatedTime) => {
+    let updatedTimeFormatted = new Date(updatedTime).toLocaleString();
     switch (cat) {
         case ESTRAGON:
             document.getElementById("estragon-location").innerText = `Estragon is ${isComingIn ? "in!" : "out!"}`;
+            document.getElementById("estragon-updated").innerText = `Last updated: ${updatedTimeFormatted}`;
             break;
         case VLAD:
             document.getElementById("vlad-location").innerText = `Vlad is ${isComingIn ? "in!" : "out!"}`;
+            document.getElementById("vlad-updated").innerText = `Last updated: ${updatedTimeFormatted}`;
             break;
     }
 }
-
-socket.on('cat movement', (content) => {
-    console.log(content);
-
-    let catName = content.name;
-
-    updatePage(catName, content.in);
-
-})
 
 socket.on('cat state', (content) => {
     console.log(content);
 
     for (const cat of content.catState) {
-        updatePage(cat.name, cat.in)
+        updatePage(cat.name, cat.isInside, cat.updatedTime)
     }
 
 })
--- server.js
+++ server.js
@@ -1,27 +1,43 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const http = require('http');
+import http from 'http';
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+import {Server} from "socket.io";
 const io = new Server(server);
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const catState = {
-    "catState": [
-        {
-            "name": "Estragon",
-            "in": true
-        },
-        {
-            "name": "Vlad",
-            "in": false
-        }
-    ]
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+import {Low, JSONFile} from "lowdb";
+const file = join(__dirname, 'db.json');
+const adaptor = new JSONFile(file);
+const db = new Low(adaptor);
+
+await db.read();
+
+class Cat {
+    constructor(name, isInside) {
+        this.name = name;
+        this.isInside = isInside;
+        this.updatedTime = new Date();
+    }
 }
+
+const vlad = new Cat("Vlad", true);
+const estragon = new Cat("Estragon", false);
+
+const cats = [vlad, estragon];
+
+db.data ||= cats;
+
+await db.write();
 
 app.use(express.static('public'))
 
 app.get("/api/cats", (req, res) => {
-    res.json(catState)
+    db.read().then(() => res.json(db.data));
 })
 
 app.get("/", (req, res) => {
@@ -35,19 +51,40 @@ app.get("/clientScript", (req, res) => {
 app.post("/api/cats", (req, res) => {
     console.log(req.query);
 
-    if (req.query.cat && req.query.in) {
-        io.emit('cat movement', {name: req.query.cat, in: (req.query.in === "true")});
-        res.sendStatus(200);
+    if (req.query.cat && (req.query.in === "true" || req.query.in === "false")) {
+        switch (req.query.cat) {
+            case "Vlad":
+            case "Estragon":
+                updateDatabase(req.query.cat, req.query.in === 'true').then(() => {
+                    io.emit('cat state', {catState: db.data});
+                    res.sendStatus(200)
+                });
+                break;
+            default:
+                res.sendStatus(400);
+        }
     } else {
-        res.sendStatus(500);
+        res.sendStatus(400);
     }
 
 
 })
 
+async function updateDatabase(cat, isInside) {
+    await db.read();
+    for (let dbCat of db.data) {
+        if (dbCat.name === cat) {
+            dbCat.isInside = isInside;
+            dbCat.updatedTime = new Date();
+            break;
+        }
+    }
+    await db.write();
+}
+
 io.on('connection', () => {
     console.log("A user connected");
-    io.emit('cat state', catState);
+    io.emit('cat state', {catState: db.data});
 })
 
 
